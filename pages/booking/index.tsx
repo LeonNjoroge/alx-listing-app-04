79287e03bfea110cfd7e1808a4861e9615822a06
LeonNjoroge <leonmuraguri@gmail.com>
2025-07-13 08:15:32 +0300

    Feat: Complete task 5

--- pages/booking/index.tsx
+++ pages/booking/index.tsx
@@ -1,23 +1,45 @@
-import BookingForm from "@/components/booking/BookingForm";
-import OrderSummary from "@/components/booking/OrderSummary";
-import CancellationPolicy from "@/components/booking/CancellationPolicy";
+import {useState} from "react";
+import axios from "axios";
 
 export default function BookingPage() {
-    const bookingDetails = {
-        propertyName: "Villa Arrecife Beach House",
-        price: 7500,
-        bookingFee: 65,
-        totalNights: 3,
-        startDate: "24 August 2024",
-    };
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        cardNumber: "",
+        expirationDate: "",
+        cvv: "",
+        billingAddress: "",
+    });
+
+    const [loading, setLoading]= useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+
+        try{
+            const response = await axios.post("/api/bookings", formData);
+            alert("Booking confirmed!");
+        } catch (error) {
+            setError("Failed to submit booking");
+        }finally {
+            setLoading(false);
+        }
+    }
+
 
     return (
-        <div className="container mx-auto p-6">
-            <div className="grid grid-cols-2 gap-6">
-                <BookingForm />
-                <CancellationPolicy />
-                <OrderSummary bookingDetails={bookingDetails} />
-            </div>
-        </div>
-    );
+        <form onSubmit={handleSubmit}>
+            {/* Form fields for booking details */}
+            <button
+            disabled={loading}>
+                {loading ? "Processing..." : "Confirm & Pay"}
+            </button>
+            {error && <p className="text-red-500">{error}</p> }
+        </form>
+    )
 }
